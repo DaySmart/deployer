1bf66add2867c7747f8d1746437b520ef4777eea
Travis Walker <travis.walker@daysmart.com>
2020-10-12 10:32:37 -0400

    new provider for the dsicollection dynamic environment service

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 const deployer = require('../lib/deployer');
+const minimist = require('minimist');
 
 const run = async () => {
-    const deploy = new deployer()
+    const args = minimist(process.argv);
+    process.argv.forEach(arg => process.argv.pop(arg));
+    const file = args._[3];
+    const command = args._[2];
+    const deploy = new deployer(command, file);
     await deploy.run();
 }
 
