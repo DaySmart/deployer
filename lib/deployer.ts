a988c2871dbd34e9046dd325a74512c65e8516cb
TAYLOR GARPOW <TAYLOR.GARPOW@daysmart.com>
2022-01-21 10:48:14 -0500

    added Remove function

--- lib/deployer.ts
+++ lib/deployer.ts
@@ -16,7 +16,7 @@ class Deployer {
 
     constructor(command: any, file: string | undefined) {
         this.file = file;
-        this.command = command;
+        this.command = process.env.PROVIDER_METHOD || command;
         if(this.file) {
             this.component = this.parseComponentTemplate(this.file);
         } else {
@@ -31,8 +31,8 @@ class Deployer {
     async run() {
         if(this.command === 'deploy') {
             await this.deploy();
-        } else if(this.command === 'destroy') {
-            await this.destroy();
+        } else if(this.command === 'remove') {
+            await this.remove();
         } else {
             this.publishResultToFrankenstack();
             throw `The command ${this.command} is not implemented`;
@@ -106,7 +106,45 @@ class Deployer {
         }
     }
 
-    async destroy() {
+    async remove() {
+        let provider
+        let providerType;
+        if(this.component.provider instanceof String) {
+           providerType = this.component.provider; 
+        } else if(this.component.provider.name) {
+            providerType = this.component.provider.name;
+        } else if(this.component.provider.Name) {
+            providerType = this.component.provider.Name
+        }
+        
+        let deployResp: any = {};
+        try {
+            switch(providerType) {
+                case 'serverless-framework':
+                    provider = new Serverless(this.component);
+                    break;
+                case 'hardcoded':
+                    provider = new HardCoded(this.component);
+                    break;
+                case 'dsicollection-dynamic-environment':
+                    provider = new DsicollectionDynamicEnvironment(this.component);
+                    break;
+                case 'cdk':
+                    provider = new CDK(this.component.name, this.component.env, this.component.provider.config, this.component.inputs, this.component.provider.account);
+                    break;
+                default:
+                    throw(`The provider ${providerType} is not implemented!`);
+            }
+
+            deployResp = await provider.remove();
+        } catch(err) {
+            throw err;
+        } finally {
+            await this.publishResultToFrankenstack(deployResp);
+            if(deployResp.exception) {
+                throw deployResp.exception;
+            }
+        }
 
     }
 
