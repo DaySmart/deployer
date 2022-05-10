c3cfe8c595cade882df07f84102021eabf54fd6c
Travis Walker <travis.walker@daysmart.com>
2022-05-10 10:42:50 -0400

    attempt to generate commandschema

--- lib/providers/serverless.ts
+++ lib/providers/serverless.ts
@@ -174,6 +174,11 @@ export class Serverless {
                 const filterSupportedOptions = require(path.resolve(serverlessPath, '../cli/filter-supported-options'));
                 const resolveProviderName = require(path.resolve(serverlessPath, '../configuration/resolve-provider-name'));
                 const providerName = resolveProviderName(configuration);
+                const resolveInput = require(path.resolve(serverlessPath, '../cli/resolve-input'));
+                let c, opt, commands, isHelpRequest, commandSchema;
+                ({ c, commands, opt, isHelpRequest, commandSchema } = resolveInput(
+                    require(path.resolve(serverlessPath, '../cli/commands-schema/aws-service'))
+                  ));
                 const variableSourcesInConfig = new Set();
                 const resolverConfiguration = {
                     serviceDir,
@@ -187,10 +192,10 @@ export class Serverless {
                         strToBool: require(path.resolve(serverlessPath, '../configuration/variables/sources/str-to-bool')),
                         sls: require(path.resolve(serverlessPath, '../configuration/variables/sources/instance-dependent/get-sls'))(),
                     },
-                    options: filterSupportedOptions(options),
-                    // fulfilledSources: new Set(['file', 'self', 'strToBool']),
-                    // propertyPathsToResolve: new Set(['provider\0name', 'provider\0stage', 'useDotenv']),
-                    // variableSourcesInConfig,
+                    options: filterSupportedOptions(options, { undefined, providerName }),
+                    fulfilledSources: new Set(['file', 'self', 'strToBool']),
+                    propertyPathsToResolve: new Set(['provider\0name', 'provider\0stage', 'useDotenv']),
+                    variableSourcesInConfig,
                 };
                 const resolveVariables = require(path.resolve(serverlessPath, '../configuration/variables/resolve'));
                 await resolveVariables(resolverConfiguration);
